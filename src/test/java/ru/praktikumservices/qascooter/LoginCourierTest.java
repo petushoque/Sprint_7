04b16e547800ba02ccca046f60b8f83c96d0784d
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-08-17 09:15:53 +0700

    Update Login Courier tests

--- src/test/java/ru/praktikumservices/qascooter/LoginCourierTest.java
+++ src/test/java/ru/praktikumservices/qascooter/LoginCourierTest.java
@@ -3,10 +3,8 @@ package ru.praktikumservices.qascooter;
 import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
@@ -53,10 +51,9 @@ public class LoginCourierTest {
                 .body("message", equalTo("Учетная запись не найдена"));
     }
 
-
     @Test
     @DisplayName("Check status code and error message of /courier/login: Invalid password")
-    @Description("A test for a negative scenario, for a request with an incorrect login, the system responds with a 404 code and an error message")
+    @Description("A test for a negative scenario, for a request with an incorrect password, the system responds with a 404 code and an error message")
     public void loginCourierWithInvalidPasswordTest(){
         CourierCredentials creds = CourierCredentials.from(courier);
         creds.setPassword("wrongpassword");
@@ -71,33 +68,37 @@ public class LoginCourierTest {
                 .body("message", equalTo("Учетная запись не найдена"));
     }
 
-    //если какого-то поля нет, запрос возвращает ошибку;
     @Test
-    @DisplayName("Check 400 status code and error message of /courier/login")
-    @Description("Login with invalid login for /courier/login endpoint")
-    public void loginCourierWithoutLoginAndCheckError(){
+    @DisplayName("Check status code and error message of /courier/login: Empty login")
+    @Description("A test for a negative scenario, for a request with an empty login, the system responds with a 404 code and an error message")
+    public void loginCourierWithoutLoginTest(){
         CourierCredentials creds = CourierCredentials.from(courier);
         creds.setLogin(null);
         Response response = courierClient.postLogin(creds);
-        response.then().log().all()
+        response.then()
+                .log()
+                .all()
                 .statusCode(400)
-                .assertThat().body("message", notNullValue())
-                .assertThat().body("message", equalTo("Недостаточно данных для входа"));
-        System.out.println("Courier didn't login. Test passed" + response.asString());
+                .assertThat()
+                .body("message", notNullValue())
+                .assertThat()
+                .body("message", equalTo("Недостаточно данных для входа"));
     }
 
-    //если какого-то поля нет, запрос возвращает ошибку;
     @Test
-    public void loginCourierWithoutPasswordAndCheckError(){
+    @DisplayName("Check status code and error message of /courier/login: Empty password")
+    @Description("A test for a negative scenario, for a request with an empty password, the system responds with a 404 code and an error message")
+    public void loginCourierWithoutPasswordTest(){
         CourierCredentials creds = CourierCredentials.from(courier);
         creds.setPassword(null);
         Response response = courierClient.postLogin(creds);
-        response.then().log().all()
+        response.then()
+                .log()
+                .all()
                 .statusCode(400)
-                .assertThat().body("message", notNullValue())
-                .assertThat().body("message", equalTo("Недостаточно данных для входа"));
-        System.out.println("Courier didn't login. Test passed" + response.asString());
+                .assertThat()
+                .body("message", notNullValue())
+                .assertThat()
+                .body("message", equalTo("Недостаточно данных для входа"));
     }
-    // Дефект. Неверный код ошибки (ошибка сервера 504)
-
 }
